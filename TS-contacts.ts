fbe33bb992adc0b04026020aef82bbda41ab5278
Makovchuk Michail <krasnodar2312@mail.ru>
2023-12-20 20:32:34 +0300

    функция проверки пользователя на администратора

--- TS-contacts.ts
+++ TS-contacts.ts
@@ -1,53 +1,76 @@
-type User = {
+enum TypeUser {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+interface User {
+  type: TypeUser.USER;
   name: string;
   age: number;
-  group?: string;
-};
+  group: string;
+}
 
-type Admin = {
+interface Admin {
+  type: TypeUser.ADMIN;
   name: string;
   age: number;
   role: string;
-};
+}
 
 type Person = User | Admin;
 
+const isAdmin = (user: Person): user is Admin => {
+  if (user.type === TypeUser.ADMIN) return true;
+  else return false;
+};
+
 const persons: Person[] = [
   {
+    type: TypeUser.USER,
     name: 'Иван Петров',
     age: 27,
     group: 'SEO-специалист',
   },
   {
+    type: TypeUser.USER,
     name: 'Марат Aляуддинов',
     age: 20,
     group: 'Музыкант',
   },
   {
+    type: TypeUser.USER,
     name: 'Ренат Aляуддинов',
     age: 22,
     group: 'Семья',
   },
   {
+    type: TypeUser.USER,
     name: 'Антон Антонов',
     age: 30,
     group: 'Друзья',
   },
   {
+    type: TypeUser.USER,
     name: 'Клайд Бэрроу',
     age: 25,
     group: 'Подельники',
   },
   {
+    type: TypeUser.ADMIN,
     name: 'Босс',
     age: 25,
     role: 'администратор',
   },
 ];
 
-const logPerson = (user: User) => {
-  console.log(`${user.name}, ${user.age}`);
+const logPerson = (person: Person) => {
+  let information: string;
+  if (isAdmin(person)) {
+    information = person.role;
+  } else {
+    information = person.group;
+  }
+  console.log(`${person.name}, ${person.age}, ${information}`);
 };
 
-console.log('Users:');
 persons.forEach(logPerson);
