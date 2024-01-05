4800c7d82d603725eed949bc92dcc1598a5ae1a6
Makovchuk Michail <krasnodar2312@mail.ru>
2024-01-05 17:50:44 +0300

    fix

--- src/lesson_4-2/index.ts
+++ src/lesson_4-2/index.ts
@@ -4,14 +4,32 @@ import { DiscountSelling2 } from './DiscountSelling2';
 
 const xiaomi = new Product('xiaomi', 50);
 const samsung = new Product('samsung', 100);
+const nokia = new Product('nokia', 150);
+const motorola = new Product('motorola', 200);
 
-const xiaomiPhone = new DiscountSelling(xiaomi);
-const samsungPhone = new DiscountSelling(samsung);
+const bike = new Product('bike', 50);
+const motorbike = new Product('motorbike', 100);
+const scooter = new Product('scooter', 150);
+const car = new Product('car', 200);
 
-const xiaomiPhone2 = new DiscountSelling2(xiaomi);
-const samsungPhone2 = new DiscountSelling2(samsung, 3);
+const xiaomiPhone = new DiscountSelling2(xiaomi, 2);
+const samsungPhone = new DiscountSelling2(samsung, 1);
+const nokiaPhone = new DiscountSelling2(nokia, 4);
+const motorolaPhone = new DiscountSelling2(motorola, 3);
 
-console.log(xiaomiPhone2);
-xiaomiPhone2.quantity = 5;
-console.log(xiaomiPhone2);
-console.log(samsungPhone2);
+const bikeTransport = new DiscountSelling(bike);
+const motorbikeTransport = new DiscountSelling(motorbike);
+const scooterTransport = new DiscountSelling(scooter);
+const carTransport = new DiscountSelling(car);
+
+
+const productList: (DiscountSelling2 | DiscountSelling)[] = [
+  bikeTransport,
+  motorbikeTransport,
+  scooterTransport,
+  carTransport,
+  xiaomiPhone,
+  samsungPhone,
+  nokiaPhone,
+  motorolaPhone,
+];
